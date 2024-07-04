afcf1509574305050f939a25d060703c0dd39705
Fz96tw <Fz96tw@hotmail.com>
2024-07-04 17:59:13 -0400

    minor code cleanup

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ provider "proxmox" {
  
 
 
-resource "proxmox_vm_qemu" "my_vm" {
- name       = "ubuntu-test2"
- agent = 0
+resource "proxmox_vm_qemu" "virtual_machine" {
+ name       = "Orinoco"
+ agent = 1
  target_node = "pve"
- clone      = "ubuntu-24.04-server-amd64-template"
+ clone      = "ubuntu-24.04-srv-amd64"
  full_clone = "true"
  #storage    = "local-lvm"
  cores      = 1
@@ -19,32 +19,36 @@ resource "proxmox_vm_qemu" "my_vm" {
  memory     = 2048
  scsihw = "virtio-scsi-pci"
  bootdisk = "scsi0"
+ boot = "order=scsi0"
+
 
 disks {
-scsi {
-    scsi0{
-        disk{
-           # slot     = 0
-            size     = "32"
-           # type     = "scsi"
-            storage  = "local-lvm"
-            # Enables thin-provisioning
-            #discard = "on"
-            #iothread = 1
+    scsi {
+        # this will just map to the boot disk in vm template
+        scsi0{
+            disk{
+            # slot     = 0
+                size     = "32" # size must match the disk in template
+            # type     = "scsi"
+                storage  = "local-lvm"
+                # Enables thin-provisioning
+                #discard = "on"
+                #iothread = 1
+            }
+        }
+        # extra disk for test purposes 
+        scsi1{
+            disk{
+            # slot     = 0
+                size     = "5"
+            # type     = "scsi"
+                storage  = "local-lvm"
+                # Enables thin-provisioning
+                #discard = "on"
+                #iothread = 1
+            }
         }
     }
-    scsi1{
-        disk{
-           # slot     = 0
-            size     = "5"
-           # type     = "scsi"
-            storage  = "local-lvm"
-            # Enables thin-provisioning
-            #discard = "on"
-            #iothread = 1
-        }
-    }
-  }
 }
 
   network {
@@ -55,7 +59,5 @@ scsi {
     link_down = false
   }
 
-
-#   boot = "order=scsi0"
 }
 

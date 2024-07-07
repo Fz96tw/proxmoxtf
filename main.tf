cdf092a8c8cedac21fca629f3217b67c6acef13a
Fz96tw <Fz96tw@hotmail.com>
2024-07-06 21:33:58 -0400

    initial commit to provision windows vm

--- main.tf
+++ main.tf
@@ -6,6 +6,59 @@ provider "proxmox" {
 }
  
 
+resource "proxmox_vm_qemu" "win2k19-vm" {
+  ## Wait for the cloud-config file to exist
+
+  name        = "Danube"
+  target_node = "pve2"
+
+  # Clone from cloudinit template
+  clone      = "danube-w2k19"
+  full_clone = true
+  #os_type = "cloud-init"
+
+
+    agent = 1
+    cores      = 1
+    sockets = 1
+    memory = 4096
+
+    scsihw = "virtio-scsi-pci"
+    bootdisk = "scsi0"
+    boot = "order=scsi0"
+
+#  disk {
+#    size    = "80G"
+#    type    = "sata"
+#    storage = "DefaultPool"
+#  }
+
+
+    disks {
+        scsi {
+            # this will just map to the boot disk in vm template
+            scsi0{
+                disk{
+                # slot     = 0
+                    size     = "32" # size must match the disk in template
+                # type     = "scsi"
+                    storage  = "local-lvm"
+                    # Enables thin-provisioning
+                    #discard = "on"
+                    #iothread = 1
+                }
+            }
+        }
+    }
+
+
+  # Set the network
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+}
+
 
 resource "proxmox_vm_qemu" "virtual_machine" {
  name       = "Orinoco"
